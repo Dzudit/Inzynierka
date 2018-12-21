3fe603d4a48ae30250aa418c41f36339b7584d5e
Dzudit <dzudit@localhost.localdomain>
2018-12-21 17:40:30 +0100

    first commit

---
+++ backend/src/api/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import CategorieViewSet
+from rest_framework.routers import DefaultRouter
+
+# urlpatterns = [
+#     path('', CategorieListView.as_view()),
+#     path('create/', CategorieCreateView.as_view()),
+#     path('<pk>', CategorieDetailView.as_view()),
+#     path('<pk>/update/', CategorieUpdateView.as_view()),
+#     path('<pk>/delete/', CategorieDeleteView.as_view()),
+# ]
+
+router = DefaultRouter()
+router.register(r'', CategorieViewSet, basename='categorie')
+urlpatterns = router.urls
---
+++ backend/src/api/api/views.py
@@ -0,0 +1,31 @@
+from rest_framework import viewsets
+from api.models import Categorie
+from .serializers import CategorieSerializer
+
+# class CategorieListView(ListAPIView):
+#     queryset = Categorie.objects.all()
+#     serializer_class = CategorieSerializer
+
+# class CategorieDetailView(RetrieveAPIView):
+#     queryset = Categorie.objects.all()
+#     serializer_class = CategorieSerializer
+
+# class CategorieCreateView(CreateAPIView):
+#     queryset = Categorie.objects.all()
+#     serializer_class = CategorieSerializer
+
+# class CategorieUpdateView(UpdateAPIView):
+#     queryset = Categorie.objects.all()
+#     serializer_class = CategorieSerializer
+
+# class CategorieDeleteView(DestroyAPIView):
+#     queryset = Categorie.objects.all()
+#     serializer_class = CategorieSerializer        
+                
+class CategorieViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = CategorieSerializer
+    queryset = Categorie.objects.all()
+
